185c422ecc37e8e0f74080e3e6ae2ffd5bac70f8
thienle2405 <dt07ethicalhcker@gmail.com>
2024-02-11 15:36:33 +0700

    create-branch-project

--- src/App.js
+++ src/App.js
@@ -3,16 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <span class="box-decoration-slice bg-gradient-to-r from-indigo-600 to-pink-500 text-white px-2 ...">
-        Hello
-        <br />
-        World
-      </span>
-      <span class="box-decoration-clone bg-gradient-to-r from-indigo-600 to-pink-500 text-white px-2 ...">
-        Hello
-        <br />
-        World
-      </span>
+      Hello World 
     </>
   );
 }
